2a4d5a21752c9233e416b2fc0deb433fab3f937b
Merle Hoyer <mhoyer06@gmail.com>
2026-01-29 14:34:39 +0000

    optimised layout

--- courses/coding-in-C/mySolutions/chessboard.c
+++ courses/coding-in-C/mySolutions/chessboard.c
@@ -7,8 +7,10 @@ int main(){
 
     for (int i=0; i<8; i ++){
         for (int j=0; j<8; j++){
-            printf("%c",column[i]);
-            printf("%d ",row[j]);;
+            
+            printf("%c",column[j]);
+            printf("%d ",row[i]);
+            
         }
         
             printf("\n");
