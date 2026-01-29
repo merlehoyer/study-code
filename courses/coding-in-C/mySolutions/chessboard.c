6855556a130bbf23a50e8c3b9d28542febed9f36
Merle Hoyer <mhoyer06@gmail.com>
2026-01-29 14:48:12 +0000

    Start of design aspect

--- courses/coding-in-C/mySolutions/chessboard.c
+++ courses/coding-in-C/mySolutions/chessboard.c
@@ -5,11 +5,21 @@ int main(){
     int row[10] = {8,7,6,5,4,3,2,1};
     char column[10]= {"ABCDEFGH"};
 
-    for (int i=0; i<8; i ++){
+
+    for (int i=0; i<10; i ++){
+       
         for (int j=0; j<8; j++){
-            
-            printf("%c",column[j]);
-            printf("%d ",row[i]);
+            if(1<i<9){
+                
+            }
+            if ((i+j)%2==0){
+                printf("|##|");
+
+            } else {
+                printf("  ");
+            }
+            //printf("%c",column[j]);
+            //printf("%d ",row[i]);
             
         }
         
